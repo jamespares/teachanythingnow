8d5d8a2488d9d0afe696cac7ba266888b2a54760
James Pares <127338534+jamespares@users.noreply.github.com>
2026-07-29 17:56:27 +0800

    chore: remove Stripe client library (service is now free)

--- src/lib/stripe.ts
+++
@@ -1,8 +0,0 @@
-import Stripe from "stripe";
-
-export function stripe(secretKey: string) {
-  return new Stripe(secretKey, {
-    apiVersion: "2025-01-27.acacia" as any,
-    typescript: true,
-  });
-}
